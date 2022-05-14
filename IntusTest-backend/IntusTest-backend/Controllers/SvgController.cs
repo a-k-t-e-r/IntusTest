e035e8b439f769e341fa334daa44759db7fe4d1d
Md. Akter Hossain <40089216+a-k-t-e-r@users.noreply.github.com>
2022-05-15 00:02:29 +0600

    Backend Added

---
+++ IntusTest-backend/IntusTest-backend/Controllers/SvgController.cs
@@ -0,0 +1,45 @@
+﻿using IntusTest_backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+
+namespace IntusTest_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SvgController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getdata")]
+        public object GetData()
+        {
+            try
+            {
+                string allPerimeter = System.IO.File.ReadAllText(@"./files/data.json");
+
+                object jsonObject = JsonConvert.DeserializeObject(allPerimeter);
+                
+                return jsonObject;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("setdata")]
+        public void SetData(SvgModel perimeters)
+        {
+            try
+            {
+                System.IO.File.WriteAllText(@"./files/data.json", JsonConvert.SerializeObject(perimeters));
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ IntusTest-backend/IntusTest-backend/Models/SvgModel.cs
@@ -0,0 +1,10 @@
+﻿namespace IntusTest_backend.Models
+{
+    public class SvgModel
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Left { get; set; }
+        public int Top { get; set; }
+    }
+}
